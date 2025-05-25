950e5e5c521f72d0f6e85e21639ea1b428937ee9
Thu Yein <thuyein1996alpha@gmail.com>
2025-05-25 14:36:27 +0700

    [Migration] Cart table relationship correction by adding cartitem list

---
+++ RestaurantManagementApp.DbService/Migrations/20250525073501_correction-cart-table1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantManagementApp.DbService.Migrations
+{
+    /// <inheritdoc />
+    public partial class correctioncarttable1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- RestaurantManagementApp.DbService/Tables/TblCart.cs
+++ RestaurantManagementApp.DbService/Tables/TblCart.cs
@@ -5,5 +5,6 @@ public class TblCart
     public Guid Id { get; set; }
     public Guid CustomerId { get; set; }
     public TblCustomer Customer { get; set; }
+    public ICollection<TblCartItem>? CartItems { get; set; }
     public int Total { get; set; }
 }
