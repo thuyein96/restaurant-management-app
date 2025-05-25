fbe65b0ec7536d5778a3b3d372534b28c14052b6
Thu Yein <thuyein1996alpha@gmail.com>
2025-05-25 14:54:53 +0700

    [Complete] Mapping cart, cartitem, user

--- RestaurantManagementApp.Extension/Extension.cs
+++ RestaurantManagementApp.Extension/Extension.cs
@@ -1,4 +1,7 @@
-﻿namespace RestaurantManagementApp.Extension;
+﻿using RestaurantManagementApp.Dtos.Features.Cart;
+using RestaurantManagementApp.Dtos.Features.User;
+
+namespace RestaurantManagementApp.Extension;
 
 public static class Extension
 {
@@ -93,6 +96,145 @@ public static class Extension
         {
             MenuItemId = menuItemCustomizeOption.MenuItemId,
             CustomizeOptionId = menuItemCustomizeOption.CustomizeOptionId
+        };
+    }
+
+    public static UserDto ToDto(this TblUser user)
+    {
+        return new UserDto
+        {
+            UserId = user.Id,
+            UserName = user.Name,
+            Email = user.Email,
+            PhoneNumber = user.PhoneNumber,
+            CreatedAt = user.CreatedAt,
+            UpdatedAt = user.UpdatedAt
+        };
+    }
+    public static CustomerDto ToDto(this TblCustomer customer)
+    {
+        return new CustomerDto
+        {
+            CustomerId = customer.Id,
+            UserId = customer.UserId,
+            User = new UserDto
+            {
+                UserId = customer.UserId,
+                UserName = customer.User.Name,
+                Email = customer.User.Email,
+                PhoneNumber = customer.User.PhoneNumber,
+                CreatedAt = customer.User.CreatedAt,
+                UpdatedAt = customer.User.UpdatedAt
+            }
+        };
+    }
+
+    public static TblCustomer ToEntity(this CustomerDto customerDto)
+    {
+        return new TblCustomer
+        {
+            Id = customerDto.CustomerId,
+            UserId = customerDto.UserId,
+            User = new TblUser
+            {
+                Id = customerDto.User.UserId,
+                Name = customerDto.User.UserName,
+                Email = customerDto.User.Email,
+                PhoneNumber = customerDto.User.PhoneNumber,
+                CreatedAt = customerDto.User.CreatedAt,
+                UpdatedAt = customerDto.User.UpdatedAt
+            }
+        };
+    }
+
+    public static AdminDto ToDto(this TblAdmin admin)
+    {
+        return new AdminDto
+        {
+            AdminId = admin.Id,
+            Password = admin.HashedPassword,
+            UserId = admin.UserId,
+            User = new UserDto
+            {
+                UserId = admin.UserId,
+                UserName = admin.User.Name,
+                Email = admin.User.Email,
+                PhoneNumber = admin.User.PhoneNumber,
+                CreatedAt = admin.User.CreatedAt,
+                UpdatedAt = admin.User.UpdatedAt
+            }
+        };
+    }
+
+    public static TblAdmin ToEntity(this AdminDto adminDto)
+    {
+        return new TblAdmin
+        {
+            Id = adminDto.AdminId,
+            HashedPassword = adminDto.Password,
+            UserId = adminDto.UserId,
+            User = new TblUser
+            {
+                Id = adminDto.User.UserId,
+                Name = adminDto.User.UserName,
+                Email = adminDto.User.Email,
+                PhoneNumber = adminDto.User.PhoneNumber,
+                CreatedAt = adminDto.User.CreatedAt,
+                UpdatedAt = adminDto.User.UpdatedAt
+            }
+        };
+    }
+
+    public static CartItemDto ToDto(this TblCartItem cartItem)
+    {
+        return new CartItemDto
+        {
+            CartItemId = cartItem.Id,
+            CartId = cartItem.CartId,
+            Cart = cartItem.Cart.ToDto(),
+            MenuItemId = cartItem.MenuItemId,
+            MenuItem = cartItem.MenuItem.ToDto(),
+        };
+    }
+
+    public static TblCartItem ToEntity(this CartItemDto cartItemDto)
+    {
+        return new TblCartItem
+        {
+            Id = cartItemDto.CartItemId,
+            CartId = cartItemDto.CartId,
+            MenuItemId = cartItemDto.MenuItemId,
+            Quantity = cartItemDto.Quantity
+        };
+    }
+    public static CartDto ToDto(this TblCart cart)
+    {
+        return new CartDto
+        {
+            CartId = cart.Id,
+            CustomerId = cart.CustomerId,
+            Total = cart.Total,
+            CartItems = cart.CartItems
+                .Select(x => x.ToDto())
+                .ToList()
+        };
+    }
+
+    public static TblCart ToEntity(this CartDto cartDto)
+    {
+        return new TblCart
+        {
+            Id = cartDto.CartId,
+            CustomerId = cartDto.CustomerId,
+            Total = cartDto.Total,
+            CartItems = cartDto.CartItems
+                .Select(x => new TblCartItem
+                {
+                    Id = x.CartItemId,
+                    CartId = x.CartId,
+                    MenuItemId = x.MenuItemId
+                })
+                .ToList()
         };
     }
 }
